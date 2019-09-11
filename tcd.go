fa7221ea2c9da0502c9f243acf36e8d0d7a4b8b2
Clay Dunston <claydunston@gmail.com>
2019-09-10 21:35:16 -0500

    Update my convenience methods

--- tcd.go
+++ tcd.go
@@ -3,11 +3,10 @@ package gofpdf
 // GetFontSize returns the current font size (not sure in what units).
 // GetFontSize returns the size of the current font in points followed by the size in the unit
 func (gp *Fpdf) GetFontSize() (float64, float64) {
-	return gp.curr.Font_Size, PointsToUnits(int(gp.curr.Font_Size), Unit_MM)
+	return gp.curr.FontSize, PointsToUnits(gp.curr.unit, gp.curr.FontSize)
 }
 
-// GetLineHeight ...
+// GetLineHeight probably won't work like I think it will.
 func (gp *Fpdf) GetLineHeight() float64 {
-	return PointsToUnits(int(gp.curr.Font_Size), Unit_MM)
-
+	return PointsToUnits(gp.curr.unit, gp.curr.FontSize)
 }
